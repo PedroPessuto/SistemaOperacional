4753a9d4d0d892e28b27b028b1668517ae424ba7
Pedro Pessuto <pedro.pessuto@outlook.com>
2024-04-18 19:02:02 -0300

    Add files via upload

---
+++ Proj1/processo.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int pessoas_globais[2][10000];
+int tamanhos[2] = {0, 0};
+int direcao;
+int tempo_atual;
+
+int escada_rolante() {
+
+  // ========== VARIÁVEIS ==========
+  int direcao_oposta;
+  int indices[2] = {0, 0};
+  int ultima_partida = 0;
+
+  tempo_atual = pessoas_globais[direcao][0];
+
+  while (indices[0] < tamanhos[0] || indices[1] < tamanhos[1]) {
+    direcao_oposta = 1 - direcao;
+
+    int pode_avancar = indices[direcao] < tamanhos[direcao];
+    int partida_valida =
+        pessoas_globais[direcao][indices[direcao]] <= ultima_partida;
+    int fim_oposto_atingido =
+        indices[direcao_oposta] == tamanhos[direcao_oposta];
+    int menor_que_oposto =
+        pessoas_globais[direcao][indices[direcao]] <
+        pessoas_globais[direcao_oposta][indices[direcao_oposta]];
+
+    if (pode_avancar &&
+        (partida_valida || fim_oposto_atingido || menor_que_oposto)) {
+      // Atualiza o tempo atual para o tempo de partida da pessoa na posição
+      // atual
+      tempo_atual = pessoas_globais[direcao][indices[direcao]];
+
+      // Avança o indice
+      indices[direcao]++;
+    } else {
+      // Determina o novo tempo atual baseado na comparação entre a última
+      // partida e a próxima pessoa na direção oposta
+      int proxima_partida_oposta =
+          pessoas_globais[direcao_oposta][indices[direcao_oposta]];
+
+      tempo_atual = (ultima_partida > proxima_partida_oposta)
+                        ? ultima_partida
+                        : proxima_partida_oposta;
+
+      // Avança o índice na direção oposta
+      indices[direcao_oposta]++;
+
+      // Atualiza o tempo de todas as pessoas na direção oposta que têm tempo
+      // menor que a última partida
+      for (int i = indices[direcao_oposta];
+           i < tamanhos[direcao_oposta] &&
+           ultima_partida > pessoas_globais[direcao_oposta][i];
+           ++i) {
+        pessoas_globais[direcao_oposta][i] = ultima_partida;
+      }
+    }
+
+    ultima_partida = tempo_atual + 10;
+    direcao = 1 - direcao;
+  }
+
+  return ultima_partida + 10;
+}
+
+int main() {
+
+  // ========== INPUT ==========
+  FILE *arquivo_entrada, *arquivo_saida;
+
+  // Abre o arquivo
+  arquivo_entrada = fopen("input.txt", "r");
+  if (arquivo_entrada == NULL) {
+    printf("Erro ao abrir o arquivo de entrada.\n");
+    return 0;
+  }
+
+  // Pega a quantidade de pessoas
+  int numero_pessoas;
+  fscanf(arquivo_entrada, "%d", &numero_pessoas);
+
+  // Pega o tempo e direção de cada pessoa e adiciona na matriz certa
+  int tempo, direcao_aux;
+  for (int i = 0; i < numero_pessoas; i++) {
+    fscanf(arquivo_entrada, "%d %d", &tempo, &direcao_aux);
+
+    // Ver por qual posição começa
+    if (i == 0) {
+      direcao = direcao_aux;
+    }
+
+    pessoas_globais[direcao_aux][tamanhos[direcao_aux]] = tempo;
+    tamanhos[direcao_aux]++;
+  }
+
+  // Fecha o arquivo
+  fclose(arquivo_entrada);
+
+  // ========== PROCESSOS ==========
+
+  pid_t pid = fork();
+
+  // Processo Filho --> Resolve o problema
+  if (pid == 0) {
+    int resultado = escada_rolante();
+    return 0;
+
+  }
+  // Processo Pai --> Cria o arquivo de saída após o filho terminar de executar
+  else {
+    wait(NULL);
+
+    // Cria o arquivo
+    arquivo_saida = fopen("output.txt", "w");
+    if (arquivo_saida == NULL) {
+      printf("Erro ao abrir o arquivo de saída.\n");
+      return 0;
+    }
+
+    // Adiciona no tempo de execução certa
+    int tempo_execucao = escada_rolante();
+    fprintf(arquivo_saida, "%d\n", tempo_execucao);
+
+    // Fecha o arquivo
+    fclose(arquivo_saida);
+  }
+
+  // ========== OUTPUT ==========
+
+  return 0;
+}
