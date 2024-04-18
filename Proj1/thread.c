0397f737f7755ce04d20aadeb57c5008c9d58650
Pedro Pessuto <pedro.pessuto@outlook.com>
2024-04-18 19:52:47 -0300

    Update thread.c

--- Proj1/thread.c
+++ Proj1/thread.c
@@ -18,43 +18,29 @@ void *escada_rolante() {
 
   while (indices[0] < tamanhos[0] || indices[1] < tamanhos[1]) {
     direcao_oposta = 1 - direcao;
-
+    
     int pode_avancar = indices[direcao] < tamanhos[direcao];
-    int partida_valida =
-        pessoas_globais[direcao][indices[direcao]] <= ultima_partida;
-    int fim_oposto_atingido =
-        indices[direcao_oposta] == tamanhos[direcao_oposta];
-    int menor_que_oposto =
-        pessoas_globais[direcao][indices[direcao]] <
-        pessoas_globais[direcao_oposta][indices[direcao_oposta]];
+    int partida_valida = pessoas_globais[direcao][indices[direcao]] <= ultima_partida;
 
-    if (pode_avancar &&
-        (partida_valida || fim_oposto_atingido || menor_que_oposto)) {
-      // Atualiza o tempo atual para o tempo de partida da pessoa na posição
-      // atual
+    if (pode_avancar && partida_valida) {
+      // Atualiza o tempo atual para o tempo de partida da pessoa na posição atual
       tempo_atual = pessoas_globais[direcao][indices[direcao]];
 
       // Avança o indice
       indices[direcao]++;
-    } else {
-      // Determina o novo tempo atual baseado na comparação entre a última
-      // partida e a próxima pessoa na direção oposta
-      int proxima_partida_oposta =
-          pessoas_globais[direcao_oposta][indices[direcao_oposta]];
+    } 
+    else {
+      // Determina o novo tempo atual baseado na comparação entre a última partida e a próxima pessoa na direção oposta
+      int proxima_partida_oposta = pessoas_globais[direcao_oposta][indices[direcao_oposta]];
 
-      tempo_atual = (ultima_partida > proxima_partida_oposta)
-                        ? ultima_partida
-                        : proxima_partida_oposta;
+      tempo_atual = (ultima_partida > proxima_partida_oposta) ? ultima_partida : proxima_partida_oposta;
 
       // Avança o índice na direção oposta
       indices[direcao_oposta]++;
 
       // Atualiza o tempo de todas as pessoas na direção oposta que têm tempo
       // menor que a última partida
-      for (int i = indices[direcao_oposta];
-           i < tamanhos[direcao_oposta] &&
-           ultima_partida > pessoas_globais[direcao_oposta][i];
-           ++i) {
+      for (int i = indices[direcao_oposta];  i < tamanhos[direcao_oposta] && ultima_partida > pessoas_globais[direcao_oposta][i];  ++i) {
         pessoas_globais[direcao_oposta][i] = ultima_partida;
       }
     }
